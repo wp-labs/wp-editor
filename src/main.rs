e3219dd031fac246601d2cbf74c4e98df59a9106
tangxiangyan <tangxiangyan@dy-sec.com>
2025-12-24 15:41:53 +0800

    feat: Add version information

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use clap::Parser;
 use wp_editer::server::start;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // 可以在这里添加其他命令行参数
+}
 
 #[tokio::main]
 async fn main() {
+    Args::parse();
     start().await.expect("启动服务器失败");
 }
